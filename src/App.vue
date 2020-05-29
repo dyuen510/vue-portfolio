e30d89dfe2fb6b9e505c87946f59783fd1272c48
David <davidyuen0305@gmail.com>
2020-05-28 18:22:55 -0700

    deleted some unwanted files and added main.js, app.vue, components folder with nav.vue.

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="app">
+        <NavBar />
+    </div>
+</template>
+
+<script>
+import NavBar from "./components/Nav.vue";
+
+export default {
+    name: "App",
+    components: {
+    NavBar
+    }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div class ='nav-bar'>
+    <h1 class = 'header'>David Yuen </h1>
+  <b-nav pills>
+    <b-nav-item active>Active</b-nav-item>
+    <b-nav-item>Link</b-nav-item>
+    <b-nav-item>Another Link</b-nav-item>
+    <b-nav-item disabled>Disabled</b-nav-item>
+  </b-nav>
+</div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header{
+  background: darkslategray;
+  color:white;
+  width:50%;
+}
+</style>
